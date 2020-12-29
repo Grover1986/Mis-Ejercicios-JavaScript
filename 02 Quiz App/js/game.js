b47bb06b0837e67c35b3922775571b5dedfc6d5b
grover1986 <elvis.cristobal.garrido@gmail.com>
2020-12-29 18:40:39 -0500

    Juego de Preguntas II

---
+++ 02 Quiz App/js/game.js
@@ -0,0 +1,110 @@
+const d = document
+const question = d.querySelector('#question') 
+const choices = Array.from(d.querySelectorAll('.choice-text'))
+const progressText = d.querySelector('#progressText') 
+const scoreText = d.querySelector('#score') 
+const progressBarFull = d.querySelector('#progressBarFull')   
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: '¿Cuánto es 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 2
+    },
+    {
+        question: '¿En qué ciudad se encuentra el edificio más alto del mundo?',
+        choice1: 'Dubai',
+        choice2: 'New York',
+        choice3: 'Shangai',
+        choice4: 'Ninguna de las anteriores',
+        answer: 1
+    },
+    {
+        question: '¿Qué porcentaje de adultos estadounidenses cree que la leche con chocolate proviene de vacas marrones?',
+        choice1: '20%',
+        choice2: '18%',
+        choice3: '7%',
+        choice4: '33%',
+        answer: 3
+    },
+    {
+        question: '¿Aproximadamente qué porcentaje de los cortes de energía en los EE.UU. son causados ​​por ardillas?',
+        choice1: '10-20%',
+        choice2: '5-10%',
+        choice3: '15-20%',
+        choice4: '30-40%',
+        answer: 1
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('end.html')
+    }
+    questionCounter++
+    progressText.innerText = `Pregunta ${questionCounter} de ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']    
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct'){
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    }) 
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score 
+}
+
+startGame()
